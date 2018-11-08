3d5081d21baba909f621c5abdb8868c47336d983
Adrian Gonzalez <adgp1997@hotmail.com>
2018-11-08 18:47:27 -0400

    'bla'

--- script_tanks.m
+++ script_tanks.m
@@ -2,6 +2,10 @@
 clear;clc
 fs = 1000;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                    SIMULACION ARCHIVO tanksSIMa.mdl                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 sim('tanksSIMa');
 
 figure(1)
@@ -17,3 +21,86 @@ title('Altura del tanque 2');
 xlabel('Tiempo (s)');
 ylabel('Altura (cm)');
 grid on
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                    SIMULACION ARCHIVO tanksPIDa.mdl                     %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Controlador P
+Kp = 1.767;
+Ti = inf;
+Td = 0;
+Ki = Kp/Ti;
+Kd = Kp*Td;
+
+
+sim('tanksPIDa');
+
+figure(1)
+plot(t,Href,t,h1);
+title('Altura del tanque 1. Controlador P.')
+xlabel('Tiempo (s)');
+ylabel('Altura (cm)');
+legend('Href','h1');
+grid on
+
+figure(2)
+plot(t,Href,t,h2);
+title('Altura del tanque 2. Controlador P.')
+xlabel('Tiempo (s)');
+ylabel('Altura (cm)');
+legend('Href','h2');
+grid on
+
+%Controlador PI
+Kp = 1,497;
+Ti = 10,014;
+Td = 0;
+Ki = Kp/Ti;
+Kd = Kp*Td;
+
+
+sim('tanksPIDa');
+
+figure(1)
+plot(t,Href,t,h1);
+title('Altura del tanque 1. Controlador PI.')
+xlabel('Tiempo (s)');
+ylabel('Altura (cm)');
+legend('Href','h1');
+grid on
+
+figure(2)
+plot(t,Href,t,h2);
+title('Altura del tanque 2. Controlador PI.')
+xlabel('Tiempo (s)');
+ylabel('Altura (cm)');
+legend('Href','h2');
+grid on
+
+%Controlador PID
+Kp = 2,273;
+Ti = 10,331;
+Td = 1,613;
+Ki = Kp/Ti;
+Kd = Kp*Td;
+
+
+sim('tanksPIDa');
+
+figure(1)
+plot(t,Href,t,h1);
+title('Altura del tanque 1. Controlador PID.')
+xlabel('Tiempo (s)');
+ylabel('Altura (cm)');
+legend('Href','h1');
+grid on
+
+figure(2)
+plot(t,Href,t,h2);
+title('Altura del tanque 2. Controlador PID.')
+xlabel('Tiempo (s)');
+ylabel('Altura (cm)');
+legend('Href','h2');
+grid on
+
